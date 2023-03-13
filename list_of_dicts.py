e6f91a8b9b3fbe0be18a6b65ebab244f6272b867
korneyka3000 <korneyburau@gmail.com>
2023-03-13 15:22:00 +0300

    удобное оформление

--- list_of_dicts.py
+++ list_of_dicts.py
@@ -1,23 +1,33 @@
-'''Дано: список dict-объектов вида вида {"key": "value"},
+"""Дано: список dict-объектов вида вида {"key": "value"},
  например
- [{"key1": "value1"},
+ [
+  {"key1": "value1"},
   {"k1": "v1", "k2": "v2", "k3": "v3"},
-   {},
-    {},
-     {"key1": "value1"},
-      {"key1": "value1"},
-       {"key2": "value2"}].
+  {},
+  {},
+  {"key1": "value1"},
+  {"key1": "value1"},
+  {"key2": "value2"}
+    ]
 
 Напишите функцию, которая удаляет дубликаты из этого списка.
  Для примера выше возвращаемое значение может быть равно
   [{"key1": "value1"}, {"k1": "v1", "k2": "v2", "k3": "v3"}, {}, {"key2": "value2"}].
-Обязательное условие: функция не должна иметь сложность O(n^2).'''
+Обязательное условие: функция не должна иметь сложность O(n^2)."""
 
 
 def remove_duplicates(lst: list) -> list:
-    return list({str(x): x for x in lst})
+    return list({str(map_obj): map_obj for map_obj in lst})
 
-lst = [{"key1": "value1"},
-       {"k1": "v1", "k2": "v2", "k3": "v3"}, {}, {}, {"key1": "value1"}, {"key1": "value1"}, {"key2": "value2"}]
-new_lst = remove_duplicates(lst)
+
+lst_of_dicts = [
+    {"key1": "value1"},
+    {"k1": "v1", "k2": "v2", "k3": "v3"},
+    {},
+    {},
+    {"key1": "value1"},
+    {"key1": "value1"},
+    {"key2": "value2"}
+]
+new_lst = remove_duplicates(lst_of_dicts)
 print(new_lst)
